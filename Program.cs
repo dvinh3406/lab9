5340d20c5ee432371557657e5ebe4257d5490385
dvinh3406 <pdv206@gmail.com>
2024-10-10 15:32:45 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("ban hay nhap vao 1 so");
+        int so = Convert.ToInt32(Console.ReadLine());
+        if(so<5)
+        {
+        switch (so)
+        {
+            case 1:
+                Console.WriteLine("*");
+                Console.WriteLine("**");
+                Console.WriteLine("***");
+                Console.WriteLine("****");
+                Console.WriteLine("*****");        
+                Console.WriteLine("******");
+                break;
+            case 2:        
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                break;
+            case 3:        
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                break;
+            case 4:        
+                Console.WriteLine("******");
+                Console.WriteLine("*====*");
+                Console.WriteLine("*====*");
+                Console.WriteLine("*====*");
+                Console.WriteLine("*====*");
+                Console.WriteLine("******");
+                break;
+            case 0:        
+                Environment.Exit(so);
+                break;
+            default:
+                Console.WriteLine("gia tri");
+                break;
+        }
+        
+        }
+        else
+        {
+            Console.WriteLine("nhap sai khoang roi");
+        }
+    }
+}
